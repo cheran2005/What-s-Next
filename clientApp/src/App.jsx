9b09a931d1108e618fa2dbae87d550db023d4a45
Cheran Balakrishnan <cheranB1@gmail.com>
2026-06-28 22:39:36 -0400

    centered whats-next logo image to correct position

--- clientApp/src/App.jsx
+++ clientApp/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
-import classes from'./App.css';
+import './App.css';
 import Logo from './assets/Whats-Next.jpg';
 function App() {
   
 
   return (
     <>
-      <img className = {classes.logo} src = {Logo} alt = "Logo pictures"/>
+      <div className = "Main">
+        <img className = "logo" src = {Logo} alt = "Logo pictures"/>
+      </div>
     </>
   )
 }
